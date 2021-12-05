ee6768973df7ec668219f3f38ea2f79022e69aa2
Monique <veldmanmonique@gmail.com>
2021-12-05 21:28:36 +0000

    Make button light up when 4 colors have been set

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,51 @@ for (i = 0; i < 4; i++) {
     colors.splice(randomColor, 1);
 }
 
-console.log(correctAnswer);
-console.log(colors);
-
 let submitColors = ['rgb(255, 17, 194)', 'rgb(137, 17, 255)', 'rgb(0, 94, 248)', 'rgb(31, 215, 61)', 'rgb(255, 217, 102)', 'rgb(255, 136, 0)'];
 
-let input = document.getElementsByClassName('input-box-circle');
+let input = document.getElementsByClassName('input-circle');
 for (i = 0; i < input.length; i++) {
-    input[i].addEventListener("click", function changeColor () {
+    input[i].addEventListener("click", function changeColor() {
         color = submitColors.shift();
         submitColors.push(color);
-    
+
         this.style.background = color;
     });
 }
 
+let circleList = [];
+let firstRow = document.getElementsByClassName('row1');
+let firstButton = document.getElementById('row1-button');
+
+for (i = 0; i < firstRow.length; i++) {
+    firstRow[i].addEventListener("click", function () {
+        checkCircleClicks(this, firstButton);
+    });
+}
+
+function checkCircleClicks(element, plug) {
+    if (element.getAttribute('disabled-button') === "false") {
+
+        console.log(element.id);
+        if (circleList.indexOf(element.id) > -1) {
+            console.log("success");
+        } else {
+            circleList.push(element.id);
+            console.log(circleList);
+            element.setAttribute('disabled-button', true);
+        }
+
+        if (circleList.length === firstRow.length) {
+            plug.style.backgroundColor = 'rgb(229, 206, 107)';
+            plug.style.boxShadow = '0 0 5px rgb(3, 0, 58)';
+            circleList = [];
+        }
+    }
+}
+
+
+
+
 document.getElementById('row1-button').addEventListener("click", function () {
-console.log(document.getElementById('i1-1').style.backgroundColor);
+    console.log(document.getElementById('i1-1').style.backgroundColor);
 })
-
-
